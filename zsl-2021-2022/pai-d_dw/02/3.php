6e146b8de532006cc4fb46c5100b58ff7df9bc96
personification-domelan <domelan10@gmail.com>
2021-09-13 07:58:45 +0200

    Inititial commit.

---
+++ zsl-2021-2022/pai-d_dw/02/3.php
@@ -0,0 +1,25 @@
+<?php
+/*
+postunkrementacja $x++
+preinkrementacja ++$x
+postdekrementacja $x--
+predekrementacja --$x
+*/
+$x=1;
+echo '<hr>', $x; //1
+echo '<br>', ++$x; //2
+echo '<br>', $x++; //2
+echo '<br>', ++$x, '<hr>'; //
+
+$x=1;
+echo $x, '<br>'; //1
+$x=$x++;
+echo $x, '<br>'; //1
+$x=++$x;
+echo $x, '<br>'; //2
+$y=++$x;
+echo $x, ",", $y, '<br>'; //3 4
+$y=$x++;
+echo $x, ",", $y, '<br>'; //3 3
+
+?>
