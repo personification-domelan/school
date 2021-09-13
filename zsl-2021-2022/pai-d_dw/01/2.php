4ace7731df0cca0ffe85457659c0502c11552ab8
personification-domelan <domelan10@gmail.com>
2021-09-13 07:53:57 +0200

    13.09.2021-7:54

--- zsl-2021-2022/pai-d_dw/01/2.php
+++ zsl-2021-2022/pai-d_dw/01/2.php
@@ -1,9 +1,23 @@
 <?php
-//php 8.0.10
-echo '<br>', 2**10, '<br';
-$x=10;
-$y=100;
-echo $x<=>$y, "<br>";
-$x=1;
-$y=1.0;
+    echo PHP_VERSION; //7.4.8
+    echo '<br>', 2**10, '<br>';
+    $x=10;
+    $y=100;
+    echo $x<=>$y, "<br>"; //porównanie, potrójny operator porównania
+
+    //równe lub identyczne
+    $x=1;
+    $y=1.0;
+    if($x==$y){
+        echo "równe";
+    }else{
+        echo "różne";
+    }
+
+    //sprawdzenie dodatkowo typu danych
+    if($x===$y){
+        echo "równe";
+    }else{
+        echo "różne";
+    }
 ?>
