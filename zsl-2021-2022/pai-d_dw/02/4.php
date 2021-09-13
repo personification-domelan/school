f9d71f91fea200ab1a2be8ea92682d709b4edd62
personification-domelan <domelan10@gmail.com>
2021-09-13 08:51:15 +0200

    13.09.2021-8:51

--- zsl-2021-2022/pai-d_dw/02/4.php
+++ zsl-2021-2022/pai-d_dw/02/4.php
@@ -7,10 +7,13 @@
     </head>
     <body>
         <main>
-            Zawartość strony:<br>
+            Zawartość strony:<hr>
             <?php 
-            require '4.1.php';            
+            include '4.1.php';
+            include_once '4.1.php';
+            require_once '4.1.php';            
             ?>
+            <hr>Koniec pliku
         </main>
     </body>
 </html>
---
+++ zsl-2021-2022/pai-d_dw/02/5.php
@@ -0,0 +1,44 @@
+<?php 
+$tekst = <<<T
+zsl - Zespół
+Szkół
+Łączności
+T;
+echo $tekst,'<br>';
+echo nl2br($tekst),'<hr>';
+
+$name="jaNuSz";
+//zmiana na małe
+echo strtolower($name),'<br>';
+//zmiana na duże
+echo strtoupper($name),'<hr>';
+
+$data="jaNUsz koWALski";
+//pierwsza na duże
+echo ucfirst($data),'<br>';
+//każda pierwsza na duże
+echo ucwords($data),'<hr>';
+
+$lorem="Excepteur ea esse nostrud nostrud exercitation. Non irure do laborum elit qui nisi exercitation commodo deserunt esse in qui ex. Culpa duis veniam cupidatat nulla dolore occaecat. Voluptate eu ut commodo sunt nostrud.";
+echo "$lorem<hr>";
+$col=wordwrap($lorem,40,'<br>');
+echo $col;
+
+//czyszczenie zawrtości buffora
+ob_clean();
+
+//usuwanie białych znaków
+$name="Anna";
+$name1="  Anna ";
+echo "Długość \$name: ",strlen($name),'<br>';//4
+echo "Długość \$name1: ",strlen($name1),'<br>';//7
+echo strlen(ltrim($name1));//5
+echo strlen(rtrim($name1));//6
+echo strlen(trim($name1));//4
+
+//przeszukiwanie ciągów
+echo strstr("janusz@gmail.com","@");//@gmail.com
+echo stristr("janusz@gmail.com","G");//gmail.com
+echo stristr("janusz@gmail.com","64");//@gmail.com
+
+?>
