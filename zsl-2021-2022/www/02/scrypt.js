5b2d26b759087287377e4a702f7e4b06837ae292
personification-domelan <domelan10@gmail.com>
2021-09-23 17:29:18 +0200

    23.09.2021-17:30

---
+++ zsl-2021-2022/www/02/scrypt.js
@@ -0,0 +1,11 @@
+/*alert("Imię");
+confirm("Nazwisko");
+let clas = prompt("Podaj klasę:","3GI2T");
+alert(clas);
+document.write("Wchodzę do klasy: "+clas);*/
+let first = prompt("Wprowadź pierwszą liczbe:","0");
+let secound = prompt("Wprowadź drugą liczbę:","0");
+var minus = first - secound;
+document.write("Wynik odejmowania to: "+minus+"<br>");
+var plus = parseInt(first) + parseInt(secound);
+document.write("Wynik dodawania to: "+plus);
