1ed3dabe0d4d72089ca67d3004b936312a40bec9
personification-domelan <domelan10@gmail.com>
2021-09-29 12:03:06 +0200

    29.09.2021-9:55

---
+++ zsl-2021-2022/inf d_dw/diagnoza/zad 3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	int XX = 0, YY = 0;
+	float d;
+	cout << "Wprowadz x punktu: "; cin >> XX;
+	cout << "\nWprowadz y punktu: "; cin >> YY;
+	d = sqrt((XX * XX) + (YY * YY));
+	if (d < 5)
+	{
+		cout << "\nPunkt zanjduje sie w okregu.";
+	}
+	else if (d > 5)
+	{		
+		cout << "\nPunkt zanjduje sie poza okregiem.";
+	}
+	else
+	{
+		cout << "\nPunkt zanjduje sie na okregu.";
+	}
+}
---
+++ zsl-2021-2022/inf d_dw/diagnoza/zad 4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int tab[18], min;
+	for (int i = 0; i < 18; i++)
+	{
+		tab[i] = rand()%30-15;
+	}
+	min = tab[0];
+	for (int i = 0; i < 18; i++)
+	{
+		if (tab[i] < min)
+		{
+			min = tab[i];
+		}
+	}
+	cout << min;
+}
