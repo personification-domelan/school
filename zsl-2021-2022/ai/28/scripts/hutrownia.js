0751c107465b837577d9200d6116abd08f88bf13
personification-domelan <domelan10@gmail.com>
2022-03-31 10:42:55 +0200

    31.03.2022 repair ai/28

---
+++ zsl-2021-2022/ai/28/scripts/hutrownia.js
@@ -0,0 +1,3 @@
+var coffeeWeight = document.getElementById("coffeeWeight");
+var coffeNumber = document.getElementById("coffeNumber");
+var coffee = {1:{name:"Kawa palona Arabica", value:5}, 2:{name:"Kawa palona Robusta",value:7}, 3:{name:"Kawa bezkofeinowa",value:6}};
