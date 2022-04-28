a5c58ad640d7673f8a40119c4ff92e1e62ddc9e2
personification-domelan <domelan10@gmail.com>
2022-04-28 10:14:47 +0200

    28.04.2022 ai/30

--- zsl-2021-2022/ai/30/script/index.js
+++ zsl-2021-2022/ai/30/script/index.js
@@ -1,16 +1,59 @@
-var table = new Array(1,2,3,4,5,6,7,8,9,10);
-var handler = document.getElementById("handler");
+var handler1 = document.getElementById("handler1");
+var handler2 = document.getElementById("handler2");
+var handler3 = document.getElementById("handler3");
+var handler4 = document.getElementById("handler4");
 
-function show() {
-    for (var i = 0; i < table.length; i++) {
-        handler.innerHTML += table[i]
-    }; handler.innerHTML += "\n";
+function show(table) {
+    for (let i = 0; i < table.length; i++) {
+        handler1.innerHTML += table[i]
+    }; handler1.innerHTML += "<br>";
 }
 
-show();
-table.push(11);
-show();
-table.unshift(0);
-show();
-table.pop();
-show();
+function zad1() {
+    let table = new Array(0,1,2,3,4,5,6,7,8,9);
+    show(table);
+    table.push("10");
+    show(table);
+    table.unshift("-1");
+    show(table);
+    table.pop();
+    show(table);
+    table.shift();
+    show(table);
+    table.reverse();
+    show(table);
+    table.sort();
+    show(table);
+    handler1.innerHTML += "<br><br>";
+}
+
+function zad2() {
+    let table = new Array(0,1,2,3,4);
+    handler2.innerHTML = table[3];
+    handler2.innerHTML += "<br><br>";
+}
+
+function zad3() {
+    let table = new Array();
+    for (let i = 0; i < 10; i++) {
+        table.push(Math.floor(Math.random()*100)+1);
+    }
+    for (let i = 0; i < table.length; i++) {
+        if (table[i]%3==0) {
+            handler3.innerHTML+="<span class='bold'>"+table[i]+"</span>; "
+        } else {
+            table[i]%5==0?handler3.innerHTML+="<span class='red'>"+table[i]+"</span>; ":handler3.innerHTML+=table[i]+"</span>; "
+        }
+    }
+    handler3.innerHTML += "<br><br>";
+}
+
+function zad4() {
+    let table1 = new Array(0,1,2);
+    let table2 = new Array(3,4,5);
+    let table3 = new Array(6,7,8);
+    let temp = table1.concat(table2, table3);
+    handler4.innerHTML=temp[Math.floor(Math.random()*9)];
+}
+
+window.onload = function() {zad1(); zad2(); zad3(); zad4();};
